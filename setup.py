c076efa8a2deb01ba8c6e6e4ab73014bf2f07963
kondrat1206 <kondrat1206@gmail.com>
2023-11-29 14:02:56 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_namespace_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='assistant',
     version='0.1.0',
@@ -7,5 +10,6 @@ setup(
     author='7Team',
     license='MIT',
     packages=find_namespace_packages(),
+    install_requires=requirements,
     entry_points={'console_scripts': ['assistant = assistant.main:main']}
 )
